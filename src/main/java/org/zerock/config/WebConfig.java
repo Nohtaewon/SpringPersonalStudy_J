ffcb787722141c4c4de83c768f7aaf24e8921ca2
Nohtaewon <gkgk713@naver.com>
2022-07-03 20:21:01 +0900

    basic setting

---
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,26 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() // 'root-context.xml' 을 대신하는 클래스
+	{
+
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		return null;
+	}
+
+}
